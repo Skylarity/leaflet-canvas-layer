b1cc5f5ae5ef5b5d38133ed78fae9ccc3ff2095f
Skyler Rexroad <skyrex1095@gmail.com>
2017-07-07 14:56:22 -0600

    BLACK MAGIC

---
+++ js/map.js
@@ -0,0 +1,77 @@
+var minHexSize = 1,
+	maxHexSize = 6
+
+var colorScale = d3.scaleQuantile()
+	.domain([minHexSize, maxHexSize])
+	.range(['rgba(0,162,200, 0.5)', 'rgba(0,71,182, 0.5)', 'rgba(163,6,201, 0.5)', 'rgba(219,10,108, 0.5)', 'rgba(221,6,18, 0.5)'])
+var sizingScale = d3.scaleLinear()
+	.domain([minHexSize, maxHexSize])
+	.range([0.1, 1])
+
+// BEGIN DATA CREATION
+var data = turf.hexGrid([-109, 37, -103, 32], 6, 'miles')
+
+data.features.map(function(feature) {
+	feature.properties = {
+		size: (Math.random() * (maxHexSize - minHexSize)) + minHexSize,
+		flooded: Math.random() > .9 ? true : false
+	}
+
+	feature.geometry = turf.transformScale(feature.geometry, sizingScale(feature.properties.size))
+
+	return feature
+})
+// END DATA CREATION
+
+var map = L.map('map').setView([34.5, -106.1], 7)
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2t5bGFyaXR5IiwiYSI6ImNpczI4ZHBmbzAwMzgyeWxrZmZnMGI5ZXYifQ.1-jGFvM11OgVgYkz3WvoNw")
+	.addTo(map)
+
+var hexLayerClass = function() {
+	this.onDrawLayer = function(info) {
+		var ctx = info.canvas.getContext('2d')
+
+		ctx.clearRect(0, 0, info.canvas.width, info.canvas.height)
+
+		ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
+
+		data.features.forEach(function(d, i) {
+			var polygons = []
+			d.geometry.coordinates.forEach(function(coords) {
+				coordSet = []
+				coords.forEach(function(coordPair) {
+					var newPair = info.layer._map.latLngToContainerPoint([coordPair[1], coordPair[0]])
+					coordSet.push([newPair.x, newPair.y])
+				})
+				polygons.push(coordSet)
+			})
+
+			ctx.beginPath()
+
+			polygons.forEach(function(coords) {
+				// console.log(coords)
+				var dWorldCoords = d.geometry.coordinates[0][0]
+				if (info.bounds.contains([dWorldCoords[1], dWorldCoords[0]])) {
+					ctx.moveTo(coords[0][0], coords[0][1])
+					coords.forEach(function(coord) {
+						// console.log(coord)
+						ctx.lineTo(coord[0], coord[1])
+					})
+				}
+			})
+
+			ctx.fillStyle = colorScale(d.properties.size)
+			ctx.fill()
+
+			if (d.properties.flooded) {
+				ctx.stroke()
+			}
+
+			ctx.closePath()
+		})
+	}
+}
+
+hexLayerClass.prototype = new L.CanvasLayer()
+var hexLayer = new hexLayerClass()
+hexLayer.addTo(map)
